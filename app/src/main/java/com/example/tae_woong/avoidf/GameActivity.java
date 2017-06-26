c603cd786bdfcaaa2789ada226ab4a2073780877
ytw1122 <ytw1122@outlook.kr>
2017-06-26 10:33:33 +0900

    the player who play this game can see the maker of this game and the tool which is used.

--- app/src/main/java/com/example/tae_woong/avoidf/GameActivity.java
+++ app/src/main/java/com/example/tae_woong/avoidf/GameActivity.java
@@ -9,8 +9,9 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Chronometer;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -19,29 +20,41 @@ import java.util.TimerTask;
  * Created by Tae_woong on 2017-06-11.
  */
 public class GameActivity extends AppCompatActivity {
+    private TextView startLabel;
     Chronometer passTime;
-    float x;
-    float y;
     private ImageView imageF;
+    private int ivY;
 
     //Screen Size
     private int imageFSize;
-    private int screenHeight;
     private int screenWidth;
+    private int screenHeight;
 
+    private int frameHeight;
+
+    private float charSize;
     private float imageFx;
     private float imageFy;
 
     private Handler handler=new Handler();
     private Timer timer=new Timer();
-
-
+    private ImageView iv;
+    private boolean action_flg=false;//동작 여부 감지
+    private boolean start_flg=false;//시작 여부 감지
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_play);
+
+        startLabel=(TextView)findViewById(R.id.startLabel);
+        iv=(ImageView)findViewById(R.id.imageView1);
         imageF=(ImageView)findViewById(R.id.imageF);
         passTime =(Chronometer)findViewById(R.id.chronometer);
+
+        imageF.setX(-80.0f);
+        imageF.setY(-80.0f);
+
+        ivY=500;
 
         WindowManager wm=getWindowManager();
         Display disp=wm.getDefaultDisplay();
@@ -51,58 +64,61 @@ public class GameActivity extends AppCompatActivity {
         screenHeight=size.y;
         //스크린 사이즈를 받아온다.
 
-        imageF.setX(-80.0f);
-        imageF.setY(-80.0f);
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        changePos();
-                    }
-                });
-            }
-        },0,20);
-        startGame();
     }//게임 화면 시작
 
     public void onDestroy(){
         super.onDestroy();
         passTime.stop();
-    }//항상 앱종료할때는 타이머를 끈다.
+    }//항상 앱종료할때는 초시계를 끈다.
     public void changePos()
     {
-        imageFy+=20;
-        if(imageF.getY()>screenHeight){
-            imageFx=(float)Math.floor(Math.random()*(screenWidth-imageF.getWidth()));
-            imageFy=-100.0f;
+        if(action_flg==true){
+            ivY-=20;
+        }else{
+            ivY+=20;
         }
-        imageF.setX(imageFx);
-        imageF.setY(imageFy);
+        iv.setY(ivY);
     }
+    public void hitCheck(){
+        float imageFXcenter=imageF.getX()+imageF.getWidth()/2;
+        float imageFYcenter=imageF.getY()+imageF.getHeight()/2;
+        if(imageFYcenter>10 && imageFYcenter<100){
+            passTime.getBase();
+            passTime.stop();
+            timer.cancel();
+            timer=null;
+        }
+    }
+    public boolean onTouchEvent(MotionEvent me){
 
-    public void startGame() {
-        final ImageView iv = (ImageView) findViewById(R.id.imageView1);
-        LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayout);
-        passTime.start();
-        //hitCheck();
-        ll.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                    case MotionEvent.ACTION_MOVE:
-                    case MotionEvent.ACTION_UP:
-                        // 이미지 뷰의 위치를 마우스 가는곳으로 옮기기
-                        x = event.getX();
-                        y = event.getY();
-                        iv.setX(x);
-                        iv.setY(y);
+        if(start_flg==false){
+            start_flg=true;
+
+            FrameLayout frame=(FrameLayout)findViewById(R.id.frame);
+            screenHeight=frame.getHeight();
+            passTime.start();
+            startLabel.setVisibility(View.GONE);
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            changePos();
+                        }
+                    });
                 }
-                return true;
+            },0,20);
+        }
+        else{
+            if(me.getAction()==MotionEvent.ACTION_DOWN){
+                action_flg=true;
+            } else if(me.getAction()==MotionEvent.ACTION_UP){
+                action_flg=false;
             }
-        });
+        }
+        return true;
+     //이미지뷰 위아래로 옮기기기
     }
-
 }
